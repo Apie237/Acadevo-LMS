e6013bfe255a25fe0d3d126650d1a79d6b67e039
Apie237 <cheforsylvanus@gmail.com>
2026-01-22 09:59:53 +0100

    replaced both client and learnhub portals api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://acadev.vercel.app/api", 
+  baseURL: "https://acadevo-server.vercel.app/api", 
   headers: {
     "Content-Type": "application/json",
   },
--- learn-hub/src/utils/api.js
+++ learn-hub/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://acadev.vercel.app/api", 
+  baseURL: "https://acadevo-server.vercel.app/api", 
 });
 
 export default api;
